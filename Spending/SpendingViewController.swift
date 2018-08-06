6dfe126f272512af5355eab9c217e43b52c4da3f
Chi Yu <cfyu@princeton.edu>
2018-08-06 14:59:14 -0700

    added input restriction and simplification for add spending page

--- Spending/SpendingViewController.swift
+++ Spending/SpendingViewController.swift
@@ -8,7 +8,35 @@
 
 import UIKit
 
-class SpendingViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+// adds a toolbar to some of the textfields
+extension UITextField {
+    func addDoneNextToolbar(onDone: (target: Any, action: Selector)? = nil, onNext: (target: Any, action:Selector)? = nil) {
+        let onDone = onDone ?? (target: self, action: #selector(doneButtonTapped))
+        let onNext = onNext ?? (target: self, action: #selector(nextButtonTapped))
+        
+        let toolbar = UIToolbar()
+        toolbar.items = [
+            UIBarButtonItem(title: "Done", style: .done, target: onDone.target, action: onDone.action),
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil),
+            UIBarButtonItem(title: "Next", style: .plain, target: onNext.target, action: onNext.action)
+        ]
+        toolbar.sizeToFit()
+        
+        self.inputAccessoryView = toolbar
+    }
+    
+    @objc func doneButtonTapped() { self.resignFirstResponder()}
+    @objc func nextButtonTapped() {
+        if let nextField = self.superview?.superview?.viewWithTag(self.tag + 1) as? UITextField {
+            nextField.becomeFirstResponder()
+        } else {
+            self.resignFirstResponder()
+        }
+    }
+}
+
+class SpendingViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
     var spending: Spending?
     
     // MARK: Data constants
@@ -33,32 +61,48 @@ class SpendingViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     
     
     // MARK: Properties
-    @IBOutlet weak var day: UITextField!
-    @IBOutlet weak var month: UITextField!
-    @IBOutlet weak var year: UITextField!
+    @IBOutlet weak var date: UITextField!
     @IBOutlet weak var amount: UITextField!
     @IBOutlet weak var type: UITextField!
     @IBOutlet weak var descript: UITextField!
     @IBOutlet weak var saveButton: UIBarButtonItem!
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // set up text fields
+        date.delegate = self
+        type.delegate = self
+        amount.delegate = self
+        descript.delegate = self
+        
+        date.tag = 0
+        type.tag = 1
+        amount.tag = 2
+        descript.tag = 3
+        
+        date.addDoneNextToolbar()
+        amount.addDoneNextToolbar()
+        type.addDoneNextToolbar()
+        
+        amount.keyboardType = UIKeyboardType.decimalPad
+        
         // set up the pickers
         let datePicker = UIDatePicker()
         let typePicker = UIPickerView()
         
-        day.inputView = datePicker
-        month.inputView = datePicker
-        year.inputView = datePicker
+        date.inputView = datePicker
         type.inputView = typePicker
 
         typePicker.delegate = self
         
         datePicker.datePickerMode = .date
         datePicker.addTarget(self, action: #selector(SpendingViewController.onDidChange(sender:)), for: .valueChanged)
+        
+        // default date to today's date
+        date.text = dateToString(date: Date())
+        
+        saveButton.isEnabled = false
     }
     
     
@@ -73,42 +117,83 @@ class SpendingViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
             return
         }
         
-        let yearText = year.text ?? ""
-        let monthText = month.text ?? ""
-        let dayText = day.text ?? ""
+        let selectedDate = stringToDate(string: date.text ?? "")
         // let typeText = type.text
         let descriptText = descript.text
         let amountValue = Double(amount.text!)!
-        let dateValue = getDate(year: yearText, month: monthText, day: dayText)
-        spending = Spending(date: dateValue, descript: descriptText, money: amountValue)
+        spending = Spending(date: selectedDate, descript: descriptText, money: amountValue)
     }
     
-    // MARK: private functions
-    // return a date object from its components
-    private func getDate(year: String, month: String, day: String) -> Date {
-        let newMonth = month.count == 1 ? "0" + month : month
-        let newDay = day.count == 1 ? "0" + day : day
-        let df = DateFormatter()
-        df.dateFormat = "yyyy-MM-dd"
-        return df.date(from: year + "-" + newMonth + "-" + newDay)!
+    // MARK: UITextFieldDelegate
+    // Hide keyboard when finished editing
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
     
-    @objc private func onDidChange(sender: UIDatePicker){
-        day.text = getComponent(component: "day", date: sender.date)
-        month.text = getComponent(component: "month", date: sender.date)
-        year.text = getComponent(component: "year", date: sender.date)
+    // update save button and title when finisehd editing
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        navigationItem.title = descript.text ?? "New Spending"
+        updateSaveButtonState()
     }
     
-    private func getComponent(component: String, date: Date) -> String {
-        let df = DateFormatter()
-        switch component {
-        case "day": df.dateFormat = "dd"
-        case "month": df.dateFormat = "LL"
-        case "year": df.dateFormat = "yyyy"
-        default: return ""
+    // limits the input to be 2 decimal points for amount
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        // only check text for amount
+        if textField.tag != 2 {
+            return true
         }
+        
+        if string.isEmpty {
+            return true
+        }
+        
+        let currentText = textField.text ?? ""
+        let replacementText = (currentText as NSString).replacingCharacters(in: range, with: string)
+        
+        return isValidDouble(text: replacementText, maxDecimal: 2)
+    }
+
+    // MARK: private functions
+    // return a date type from a string
+    private func dateToString(date: Date) -> String {
+        let df = DateFormatter()
+        df.dateFormat = "MMM dd, yyyy"
         return df.string(from: date)
     }
     
-
+    // return a string from a date type
+    private func stringToDate(string: String) -> Date {
+        let df = DateFormatter()
+        df.dateFormat = "MMM dd, yyyy"
+        return df.date(from: string)!
+    }
+    
+    // when a new date is selected from the datePicker
+    @objc private func onDidChange(sender: UIDatePicker){
+        date.text = dateToString(date: sender.date)
+    }
+    
+    // update save button state
+    private func updateSaveButtonState() {
+        // Disable save button if type or amount is empty
+        let amountText = amount.text ?? ""
+        let typeText = type.text ?? ""
+        saveButton.isEnabled = !(amountText.isEmpty || typeText.isEmpty)
+    }
+    
+    // check if a double is a valid max n decimal-place double
+    private func isValidDouble(text: String, maxDecimal: Int) -> Bool {
+        let nf = NumberFormatter()
+        let separator = nf.decimalSeparator ?? "."
+        
+        if nf.number(from: text) != nil {
+            let split = text.components(separatedBy: separator)
+            let digits = split.count == 2 ? split.last ?? "" : ""
+            return digits.count <= maxDecimal
+        }
+        
+        return false
+    }
 }
